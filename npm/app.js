b7b73d29684e8debce17be6072c051e0dc10b52e
JKS5 <jokekhanson@gmail.com>
2023-07-10 19:13:17 +0900

    [feat-npm version, License,custom script command]

---
+++ npm/app.js
@@ -0,0 +1,14 @@
+console.log('hi');
+// package.json에서
+// 일반적인 명령어를 사용하고 싶다면
+
+// 내가 node.js에서 제공하는 command (ex: start) 라면
+// npm 'command'
+
+// 내가 원하는 custome command를 작성한후 실행하고 싶다면 'run'을 적어줘야함
+// npm run '나의command'를 치기
+
+// package.json의 license들 검색
+//보통은 ISC License를 많이 사용
+//https://spdx.org/licenses/
+// https://www.olis.or.kr/license/Detailselect.do?lType=spdx&lId=1074
