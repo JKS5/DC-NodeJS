14487f7a65c1bea420fda6790dbb3a5d03d70f31
JKS5 <jokekhanson@gmail.com>
2023-07-22 20:01:48 +0900

    [feat]jwt

---
+++ 9-auth/2-jwt.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const secret = '0puK^#4iaE1P26JJBo2Q4Bg0cmMCDZOf';
+//secret key https://www.lastpass.com/features/password-generator#generatorTool 권고되는 사이즈는 32byte = 256bit
+const token = jwt.sign(
+  {
+    id: 'userId',
+    isAdmin: false,
+  },
+  secret,
+  { expiresIn: 2 }
+);
+setTimeout(() => {
+  jwt.verify(token, secret, (error, decode) => {
+    console.log(error, decode);
+  });
+}, 3000);
