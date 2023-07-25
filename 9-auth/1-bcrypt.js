de3a8a08f3c71ccf4dca0ddb5e338f2fa0cd5bc3
JKS5 <jokekhanson@gmail.com>
2023-07-25 21:24:20 +0900

    [feat]-auth bcrypt,jwt

--- 9-auth/1-bcrypt.js
+++ 9-auth/1-bcrypt.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
 
 const password = 'abcd1234';
-const hashed = bcrypt.hashSync(password, 10);
+const hashed = bcrypt.hashSync(password, 12);
 console.log(`password:${password}, hashed: ${hashed}`);
+// 해쉬hased 내부에 slat와cost값 모두 들어 있기 때문에 비교가
+const result = bcrypt.compareSync(password, hashed);
+console.log(result);
